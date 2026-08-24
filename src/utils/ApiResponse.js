c6d68bf9bdffe4a8a796d857ce3324a74f790413
SindhuPriya1842007 <narsingsindhu@gmail.com>
2026-08-24 23:56:57 +0530

    utils

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,6 +1,6 @@
 class Apiresponse {
     constructor(statusCode, data = null, message = "Success", success = true, errors = []) {
-        this.statusCode = statusCode < 400 ;
+        this.statusCode = statusCode < 400 ? statusCode : 500; // Ensure statusCode is less than 400, otherwise default to 500
         this.data = data;
         this.message = message;
         this.success = success;
